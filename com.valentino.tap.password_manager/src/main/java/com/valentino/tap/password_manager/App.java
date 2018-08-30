ea0ba872c27a944308c96fed3e88a5ed7ea0a188
valentino-marano <valentino.marano@stud.unifi.it>
2018-08-30 23:59:42 +0200

    Creazione Progetto Password Manager per esame Tecniche Avanzate di Programmazione

---
+++ com.valentino.tap.password_manager/src/main/java/com/valentino/tap/password_manager/App.java
@@ -0,0 +1,7 @@
+package com.valentino.tap.password_manager;
+
+public class App {
+	public String sayHello() {
+		return "Hello";
+	}
+}
---
+++ com.valentino.tap.password_manager/src/test/java/com/valentino/tap/password_manager/AppTest.java
@@ -0,0 +1,21 @@
+package com.valentino.tap.password_manager;
+
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+	private App app;
+	@Before
+	public void setup() {
+		app = new App();
+	}
+
+	@Test
+	public void testSayHello() {
+		assertEquals("Hello", app.sayHello());
+	}
+}
